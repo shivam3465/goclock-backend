034df5408764be1c47c1445ac3834173423e752f
shivam kumar <shivamkr19102000@gmail.com>
2023-06-15 14:18:21 +0530

    fixing cookies

--- utils/setCookies.js
+++ utils/setCookies.js
@@ -5,6 +5,8 @@ export const setCookies = (res, id = "", time = 0, message = "") => {
   res.cookie("token", token, {
     expires: new Date(Date.now() + time),
     httpOnly: true,
+    sameSite: "none",
+    secure: true,
   });
   res.status(200).json({ success: true, message: message });
 };
